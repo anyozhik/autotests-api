0aba3d7dddd723f8061500362f02bd147f61317d
anyozhik <ateslyuk5@gmail.com>
2025-11-11 14:59:58 +0700

    Практика работы с TCP/IP протоколом

--- websocket_users_client.py
+++ websocket_users_client.py
@@ -6,13 +6,10 @@ async def client():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         message = "Привет, сервер!"
-        print(f"Отправка: {message}")
         await websocket.send(message)
 
     for _ in range(5):
-        response = await websocket.recv()
-        print(f"Ответ от сервера: {response}")
+        message = await websocket.recv()
+        print(message)
 
 asyncio.run(client())
-
-
