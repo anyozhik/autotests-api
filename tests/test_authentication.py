ca685538614b97181aa20746bc34e1d6ec695f6c
anyozhik <ateslyuk5@gmail.com>
2026-01-05 23:01:15 +0700

    Фикстуры pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,33 +1,26 @@
 from http import HTTPStatus
-from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
+import pytest
 
+@pytest.mark.regression
+@pytest.mark.authentication
+def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
+    request = LoginRequestSchema(email=function_user.email,password=function_user.password)
 
-def test_login():
+    response = authentication_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    public_users_client = get_public_users_client()
-    authentication_client = get_authentication_client()
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(create_user_request)
-
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-
-    login_response = authentication_client.login_api(login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
-
-    validate_json_schema(instance=login_response.json(), schema=login_response_data.model_json_schema())
+    validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
 
 
 
