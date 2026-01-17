1a360040805cd6dfce191d7d8f33106b1c12b95b
anyozhik <ateslyuk5@gmail.com>
2026-01-17 15:17:43 +0700

    Практикуемся в написании API автотестов. часть 2

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -18,6 +18,13 @@ class CourseSchema(BaseModel):
     created_by_user: UserSchema = Field(alias="createdByUser")
 
 
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
+
+
 class GetCoursesQuerySchema(BaseModel):
     """
     Описание структуры запроса на получение списка курсов.
@@ -45,13 +52,20 @@ class UpdateCourseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
     title: str | None = Field(default_factory=fake.sentence)
-    maxScore: int | None = Field(alias="maxScore", default_factory=fake.max_score)
-    minScore: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
-    estimatedTime: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 class CreateCourseResponseSchema(BaseModel):
     """
     Описание структуры ответа создания курса.
+    """
+    course: CourseSchema
+
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
     """
     course: CourseSchema
